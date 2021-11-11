95b9291334ceac7c78a9bcd21fbba3e32df3785c
arnold <nganizyo@gmail.com>
2021-11-11 18:15:06 +0000

    commit tree 36afd027d7a0e3684eb7f3eed760c5f9094f1613

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :authors, only: [:edit, :update, :show, :new, :create]
+resources :posts, only: [:edit, :update, :show, :new, :create]
 end
